ad1fc88216f6ee904c5a3b592aff90ae6daca61c
jhuber <jhuber@htl-steyr.ac.at>
2022-03-25 08:46:04 +0100

    DigitalViewGUI, DateViewGUI, WeatherViewGUI

--- src/main/java/View/DateViewGUI.java
+++ src/main/java/View/DateViewGUI.java
@@ -2,15 +2,26 @@ package View;
 
 
 import javafx.scene.control.Label;
-
+/**
+ * @author jhuber
+ * Ändert den Text des Labels, welches das Datum ausgibt.
+ */
 public class DateViewGUI extends ViewInterface {
 
     protected Label l;
 
+    /**
+     * Konstruktor
+     * @param l das Label was zu ändern ist
+     */
     public DateViewGUI(Label l) {
         this.l = l;
     }
 
+    /**
+     * Ändert das Label
+     * @param date Datum auf das geändert werden soll
+     */
     public void display(String date){
         l.setText(date);
     }
--- src/main/java/View/WeatherViewGUI.java
+++ src/main/java/View/WeatherViewGUI.java
@@ -2,15 +2,26 @@ package View;
 
 
 import javafx.scene.control.Label;
-
+/**
+ * @author jhuber
+ * Ändert den Text des Labels, welches das Wetter ausgibt.
+ */
 public class WeatherViewGUI extends ViewInterface {
 
     protected Label l;
 
+    /**
+     * Konstruktor
+     * @param l das Label was zu ändern ist
+     */
     public WeatherViewGUI(Label l) {
         this.l = l;
     }
 
+    /**
+     * Ändert das Label
+     * @param weather Wetter auf das geändert werden soll
+     */
     public void display(String weather){
         l.setText(weather);
     }
